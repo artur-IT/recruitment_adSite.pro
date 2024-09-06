eab952cc533082fffbdc640a54ce280079b2cf41
Artur Matysiak <inz.matys@gmail.com>
2024-09-06 13:41:42 +0200

    create mobile view - Hero One

--- src/App.jsx
+++ src/App.jsx
@@ -24,23 +24,25 @@ export default function App() {
         </nav>
 
         {/* Hero One */}
-        <section className="relative h-[825px] bg-[#F7F7F7] p-16">
+        <section className="relative h-[600px] lg:h-[825px] bg-[#F7F7F7] mt-5 lg:mt-0 p-5 lg:p-16">
           <div>
             {/* bg element */}
-            <div className="absolute w-1/2 max-w-[668px] h-auto top-[-10%] right-[5%] xl:right-20">
-              <img src="./img/HERO_Element.png" alt="cars" />
+            <div className="absolute invisible lg:visible w-1/2 max-w-[668px]  top-[-10%] right-[5%] xl:right-20">
+              <img className="" src="./img/HERO_Element.png" alt="cars" />
             </div>
 
             {/* info */}
             <div className="absolute z-40 w-11/12 h-[343px]">
-              <div className="grid gap-6">
-                <div className="font-bebas-neue leading-tight w-[650px] text-[76px]">{"Sprzedajemy samochody z europy".toUpperCase()}</div>
+              <div className="grid lg:gap-6">
+                <div className="font-bebas-neue leading-tight lg:w-[650px] text-5xl lg:text-[76px]">
+                  {"Sprzedajemy samochody z europy".toUpperCase()}
+                </div>
                 <p className="my-10 font-roboto-condensed text-base w-[280px] text-[#282828]">
                   Kup komfortowy pojazd, aby każda podróż była wyjątkowym przeżyciem.
                 </p>
                 <div className="flex flex-col sm:flex-row gap-6 sm:gap-6 font-roboto-condensed">
-                  <button className="w-full sm:w-[158px] h-[47px] rounded-lg bg-[#0147FF] text-[#F7F7F7]">Zobacz zdjęcia</button>
-                  <button className="w-full sm:w-[158px] h-[47px] rounded-lg bg-[#F7F7F7] text-[#0147FF] border-[#0147FF] border-[1px]">
+                  <button className="w-[158px] h-[47px] rounded-lg bg-[#0147FF] text-[#F7F7F7]">Zobacz zdjęcia</button>
+                  <button className="w-[158px] h-[47px] rounded-lg bg-[#F7F7F7] text-[#0147FF] border-[#0147FF] border-[1px]">
                     Zadzwoń do nas
                   </button>
                 </div>
@@ -48,8 +50,7 @@ export default function App() {
             </div>
 
             {/* cars */}
-            {/* <div className="absolute w-[1064px] h-[auto] left-[376px] top-[328px]"> */}
-            <div className="absolute w-3/4 max-w-[1064px] h-auto left-1/4 top-1/3 xl:left-[376px] xl:top-[328px]">
+            <div className="absolute w-3/4 max-w-[1064px] h-auto left-1/4 bottom-24 lg:top-1/3 xl:left-[376px] xl:top-[328px]">
               <img src="./img/HERO_cars.png" alt="cars" />
             </div>
           </div>
